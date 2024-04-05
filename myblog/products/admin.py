f42ae0cf7a5cada79442c27669ac48238b7e3929
ferrchazarreta <f.chazarreta@itecriocuarto.org.ar>
2024-04-05 20:01:39 -0300

    Agregado de Category

--- myblog/products/admin.py
+++ myblog/products/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from products.models import Product
+from products.models import Product, Category
 
 # Register your models here.
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name','description','price')
+    list_display = ('name',
+                    'description',
+                    'price',
+                    'category',
+    )
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',
+    )
