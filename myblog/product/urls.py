e99dda3a17a2a6b2f1d9c5ae1baf0bb93462ea5d
ferrchazarreta <f.chazarreta@itecriocuarto.org.ar>
2024-05-14 22:46:31 -0300

    Agregado de repositories y templates Category

--- myblog/product/urls.py
+++ myblog/product/urls.py
@@ -7,13 +7,24 @@ from product.views.product_view import (
     product_detail,
     product_update,
 )
-from product.views.category_view import category_list
+from product.views.category_view import (
+    category_list,
+    category_create,
+    category_delete,
+    category_update,
+)
 
 urlpatterns = [
+    # Rutas de productos
     path(route='', view=product_list, name='product_list'),
-    path(route='create/',view=product_create, name='product_create'),
-    path(route='<int:id>/',view=product_detail,name="product_detail"),
-    path(route='<int:id>/update/',view=product_update,name="product_update"),
-    path(route='<int:id>/delete/',view=product_delete,name="product_delete"),
-    path(route='category/', view=category_list, name='category_list')
+    path(route='product/create/', view=product_create, name='product_create'),
+    path(route='product/<int:id>/', view=product_detail, name='product_detail'),
+    path(route='product/<int:id>/update/', view=product_update, name='product_update'),
+    path(route='product/<int:id>/delete/', view=product_delete, name='product_delete'),
+    
+    # Rutas de categorías
+    path(route='category/', view=category_list, name='category_list'),
+    path(route='category/create/', view=category_create, name='category_create'),
+    path(route='category/<int:id>/update/', view=category_update, name='category_update'),
+    path(route='category/<int:id>/delete/', view=category_delete, name='category_delete'),
 ]
--- myblog/product/views/category_view.py
+++ myblog/product/views/category_view.py
@@ -2,13 +2,69 @@ from django.shortcuts import render, redirect
 
 from product.repositories.category import CategoryRepository
 
+repo = CategoryRepository()
+
 def category_list(request):
-    category_repository = CategoryRepository()
-    categorias = category_repository.get_all()
+    categorias = repo.get_all()
     return render(
         request,
         'categories/list.html',
         dict(
             categories=categorias
         )
+    )
+
+def category_list(request):
+    categorias = repo.get_all()
+    return render(
+        request,
+        'categories/list.html',
+        dict(
+            categories=categorias
+        )
+    )
+
+def category_delete(request, id):
+    categoria = repo.get_by_id(id=id)
+    repo.delete(categoria=categoria)
+    return redirect('category_list')
+
+def category_update(request, id):
+    category = repo.get_by_id(id=id)
+    if request.method == "POST":
+        name = request.POST.get('name')
+        repo.update(
+            category,
+            nombre=name,
+        )
+        return redirect('category_list')
+
+    return render(
+        request,
+        'categories/update.html',
+        dict(
+            category=category
+        )
+    )
+
+def category_create(request):
+    categorias = repo.get_all()
+    if request.method == "POST":
+        name = request.POST.get('name')
+        repo.create(
+            nombre=name,
+        )
+        return redirect('category_list')
+    return render (
+        request,
+        'categories/create.html',
+        dict(
+            categories=categorias
+        )
+    )
+
+def index_view(request):
+    return render(
+        request,
+        'index/index.html'
     )
